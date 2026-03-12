a9de0af032000eb6c3569f6564b62490e3ea60c5
Aaqil khan <aaqilpro99@gmail.com>
2026-03-12 15:04:03 +0530

    HEAD: Added google verification html file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -96,6 +96,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="google-site-verification" content="LgyFE7mbysgRKfm4khYfITpcJykfggm9gHxia6DTI3g" />
         <script
           type="application/ld+json"
           dangerouslySetInnerHTML={{ __html: JSON.stringify(jsonLd) }}
